1a73defe22adee00cd352712fe5e41d8d268c016
NeethumolSunny <kneethumolsunny@gmail.com>
2025-12-12 11:23:03 +0000

    Add task1 and task2 files

---
+++ task1.py
@@ -0,0 +1,12 @@
+"""TAKING TWO NUMBER AS INPUT AND PERFORMING MATHAMAATICAL OPERATIONS AND DISPLAYING THE RESULTS ON SCREEN
+ let a and b as two numbers:"""
+a=int(input("Enter the first number: "))
+b=int(input("Enter  the second number: "))
+c=a+b
+print("ADDITION:" ,c)
+d=a-b
+print("SUBTRACTION:", d)
+e=a*b
+print("MULTIPLICATION:", e)
+f=a//b
+print("DIVISION:", f)
---
+++ task2.py
@@ -0,0 +1,9 @@
+"""
+1. taking users 1st & last name as input
+2. concating 1st & last name into full name
+3. printing personalized greetings by full name
+"""
+a=input("Enter your first name: ")
+b=input("Enter your last name: ")
+c=a+b
+print("Hello ,", c ,"!"," Welcome to the pyhton program." )
